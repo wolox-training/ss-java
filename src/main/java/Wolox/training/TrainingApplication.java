f881fba7ecea0fe054887324d40a35f124cf3eea
Sebastian Sopo Martinez <ssopo@MacBook-Air.local>
2020-11-19 14:10:22 -0500

    Create new class for Book model

--- src/main/java/Wolox/training/TrainingApplication.java
+++ src/main/java/Wolox/training/TrainingApplication.java
@@ -2,7 +2,9 @@ package Wolox.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan("Wolox.training.models")
 @SpringBootApplication
 public class TrainingApplication {
 
---
+++ src/main/java/Wolox/training/models/Book.java
@@ -0,0 +1,134 @@
+package Wolox.training.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
+
+@Entity
+public class Book {
+
+    @PersistenceConstructor
+    public Book(Integer id, String genre, String author, String image, String title,
+            String subtitle,
+            String publisher, String year, Integer pages, String isbn) {
+        this.id = id;
+        this.genre = genre;
+        this.author = author;
+        this.image = image;
+        this.title = title;
+        this.subtitle = subtitle;
+        this.publisher = publisher;
+        this.year = year;
+        this.pages = pages;
+        this.isbn = isbn;
+    }
+
+    public Book() {
+        super();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column(nullable = true)
+    private String genre;
+    @Column(nullable = false)
+    private String author;
+    @Column(nullable = false)
+    private String image;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String subtitle;
+    @Column(nullable = false)
+    private String publisher;
+    @Column(nullable = false)
+    private String year;
+    @Column(nullable = false)
+    private Integer pages;
+    @Column(nullable = false)
+    private String isbn;
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public void setPages(Integer pages) {
+        this.pages = pages;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
