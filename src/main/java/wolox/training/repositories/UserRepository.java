e82e189f6acb5cb580f3c623a2d2ca4ed10263f0
Sebastian Sopo Martinez <sebastian.sopo@wolox.co>
2020-11-30 14:39:22 -0500

    Adjust issues reporting in github for query in repository

--- src/main/java/wolox/training/repositories/UserRepository.java
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -2,7 +2,6 @@ package wolox.training.repositories;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import wolox.training.models.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -21,6 +20,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
      * @param userName
      * @return
      */
-    @Query(value = "SELECT u FROM User u WHERE u.userName = ?1")
-    User findByUserName(String userName);
+    Optional<User> findByUserName(String userName);
 }
